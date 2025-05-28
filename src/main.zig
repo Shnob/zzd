7b791f518a3e57f15ee5ac7c9f1192a93489047a
Shnob <jake.dombnz@gmail.com>
2025-05-29 00:23:17 +1200

    Basic stdin to stdout implementation
    Produces exact same result as xxd (with no arguments) on the build.zig
    file (excluding text coloring).

--- src/main.zig
+++ src/main.zig
@@ -4,17 +4,68 @@ const clap = @import("clap");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
 
-    const parameters = try getParameters(gpa.allocator());
+    // Collect all the parameters supplied by the user into a struct.
+    const parameters = try getParameters(allocator);
+    _ = parameters;
 
-    const input = try getInputReader(parameters);
-    const reader = input.reader;
-    defer input.close();
+    // Setup up for reading.
+    // TODO: Replace with system that can read from stdin or a file.
+    const stdin_reader = std.io.getStdIn().reader();
+    var br = std.io.bufferedReader(stdin_reader);
+    const reader = br.reader();
 
-    var buf: [64]u8 = undefined;
-    const n = try reader.read(buf[0..]);
+    // Setup up for writing.
+    // TODO: Replace with system that can write to stdout or a file.
+    const stdout_writer = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_writer);
+    const writer = bw.writer();
 
-    std.debug.print("Read: {s}\n", .{buf[0..n]});
+    // Stores the current line of bytes to be formatted and printed.
+    // TODO: Make size of buffer (and all downstream buffers) variable.
+    var buf: [16]u8 = undefined;
+
+    // Number of characters actually read. The buffer will not always be filled
+    var n: usize = undefined;
+
+    // Index of first byte of line in the file.
+    var index: u32 = 0;
+    while (true) : (index += 16) {
+        n = try reader.read(buf[0..]);
+        if (n == 0) {
+            break;
+        }
+
+        // Make a copy of buf that we can modify for printing.
+        // This forms the ascii representation on the right hand side of the output.
+        var ascii: [16]u8 = undefined;
+        @memcpy(ascii[0..n], buf[0..n]);
+
+        // Remove things line '\n' and '\t' or else it will distort our formatting.
+        sanitizeAscii(&ascii);
+
+        // Print index information on the left of the line (in hex).
+        try writer.print("{x:0>8}: ", .{index});
+
+        // Iterate through each byte and print the hex representation.
+        // A space will break up line into pairs of bytes, for readability
+        for (buf, 0..) |byte, i| {
+            const known_byte = i < n;
+            const space = if (i % 2 == 1) " " else "";
+
+            if (known_byte) {
+                try writer.print("{x:0>2}{s}", .{ byte, space });
+            } else {
+                try writer.print("  {s}", .{space});
+            }
+        }
+
+        // Print the ascii version of the bytes on the right of the line.
+        try writer.print(" {s}\n", .{ascii[0..n]});
+
+        try bw.flush();
+    }
 }
 
 fn getParameters(allocator: std.mem.Allocator) !ZzdParameters {
@@ -29,6 +80,7 @@ fn getParameters(allocator: std.mem.Allocator) !ZzdParameters {
         .diagnostic = &diag,
         .allocator = allocator,
     }) catch |err| {
+        // TODO: Show the help page instead of this error when receiveing malformed arguments.
         diag.report(std.io.getStdErr().writer(), err) catch {};
         return err;
     };
@@ -37,55 +89,32 @@ fn getParameters(allocator: std.mem.Allocator) !ZzdParameters {
     return ZzdParameters{
         // If a file was supplied, set that as the input,
         // else, set stdin as the input.
-        .input = if (res.args.infile) |f| ZzdInput{ .file = f } else ZzdInput.stdin,
-        .output = if (res.args.outfile) |f| ZzdOutput{ .file = f } else ZzdOutput.stdout,
+        .input = if (res.args.infile) |f| ZzdParameters.Input{ .file = f } else ZzdParameters.Input.stdin,
+        .output = if (res.args.outfile) |f| ZzdParameters.Output{ .file = f } else ZzdParameters.Output.stdout,
     };
 }
 
-const ZzdInputReader = struct {
-    reader: std.io.AnyReader,
-    file: ?std.fs.File = null,
-
-    const Self = @This();
-
-    fn close(self: Self) void {
-        if (self.file) |file| {
-            file.close();
+/// Replaces characters that would produce unwanted effects, such as '\n' with '.'
+/// TODO: Currently not exhaustive. Add all cases.
+fn sanitizeAscii(string: []u8) void {
+    for (string) |*char| {
+        if (char.* == '\n' or char.* == '\t') {
+            char.* = '.';
         }
-    }
-};
-
-fn getInputReader(parameters: ZzdParameters) !ZzdInputReader {
-    switch (parameters.input) {
-        ZzdInput.stdin => {
-            std.debug.print("STDIN\n", .{});
-            return ZzdInputReader{
-                .reader = std.io.getStdIn().reader().any(),
-            };
-        },
-        ZzdInput.file => |f| {
-            std.debug.print("FILE: {s}\n", .{f});
-            const file = try std.fs.cwd().openFile(f, .{});
-
-            return ZzdInputReader{
-                .reader = file.reader().any(),
-                .file = file,
-            };
-        },
     }
 }
 
-const ZzdInput = union(enum) {
-    stdin: void,
-    file: []const u8,
-};
-
-const ZzdOutput = union(enum) {
-    stdout: void,
-    file: []const u8,
-};
-
 const ZzdParameters = struct {
-    input: ZzdInput,
-    output: ZzdOutput,
+    input: Input,
+    output: Output,
+
+    const Input = union(enum) {
+        stdin: void,
+        file: []const u8,
+    };
+
+    const Output = union(enum) {
+        stdout: void,
+        file: []const u8,
+    };
 };
